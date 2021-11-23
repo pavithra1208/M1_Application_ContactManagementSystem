47d8b3136cbd0e41a9bd77ec65071430e9260127
pavithra1208 <88384193+pavithra1208@users.noreply.github.com>
2021-11-23 19:15:06 +0530

    Update contact_management.h

---
+++ 3_Implementation/contact_management.h
@@ -0,0 +1,30 @@
+
+/**
+ * @file contact_management.h
+ * @author pavithra
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef CONTACTMANAGEMENT_H
+#define CONTACTMANAGEMENT_H
+
+
+struct dir
+{
+    char name[20];
+    char number[10];
+};
+ 
+  
+/* Function Prototype */
+void insert(char *a);
+void update(char *a);
+void delet(char *a);
+void display(char *a);
+void search(char *a);
+  
+#endif //#ifndef CONTACTMANAGEMENT_H
